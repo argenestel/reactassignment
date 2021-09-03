0dae5efb5e4f7052722d5cd4c5cb11c509c18271
argenestel <49126445+argenestel@users.noreply.github.com>
2021-09-03 13:34:27 +0530

    added changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import App from "./components/App";
 ReactDOM.render(
 	<React.StrictMode>
 		<noscript>You need to enable JavaScript to run this app.</noscript>
-
 		<App />
 	</React.StrictMode>,
 	document.querySelector("body")
